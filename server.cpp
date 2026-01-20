b1203c94a8dde57cbbe1d4d7a61cbc79ecc2d5b6
J Barrios <47373671+jbarrios7@users.noreply.github.com>
2026-01-20 15:27:20 -0500

    Add new constructor and namespace prefixes

--- server.cpp
+++ server.cpp
@@ -11,15 +11,21 @@ private:
 	struct sockaddr_in *server_addr;
 
 public:
-	websocket() : socket(socket(PF_INET, SOCK_DGRAM);) {
+	websocket() : socket(::socket(PF_INET, SOCK_DGRAM);) {
 		server_addr->sin_family = AF_INET;
 		server_addr->sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 		server_addr->sin_port = 0;
 	}
+	
+	websocket(uint16_t p) : socket(::socket(PF_INET, SOCK_DGRAM);) {
+		this->port(p);
+		server_addr->sin_family = AF_INET;
+		server_addr->sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	}
 
 	int port(uint16_t p) {
 		if (p < 1024) {
-			cout << "Invalid port number.";
+			std::cout << "Invalid port number." << std::endl;
 			return -1;
 		}
 		this->server_addr->sin_port = htons(p);
@@ -30,12 +36,12 @@ public:
 
 	int web_connect() {
 		if (this->server_addr->sin_port == 0) { 
-			cout << "Connection port not set! Use port() method.";
+			std::cout << "Connection port not set! Use port() method." << std::endl;
 			return -1; 
 		}
-		disconnectx(this->socket, SAE_ASSOCID_ANY, SAE_CONNID_ANY);
-		if (connect(this->socket, this->(const struct sockaddr*)server_addr, sizeof(*server_addr)) == -1) {
-			cout << "Connection could not be established.";
+		::disconnectx(this->socket, SAE_ASSOCID_ANY, SAE_CONNID_ANY);
+		if (::connect(this->socket, this->(const struct sockaddr*)server_addr, sizeof(*server_addr)) == -1) {
+			std::cout << "Connection could not be established." << std::endl;
 		}
 		return 0;
 	}
